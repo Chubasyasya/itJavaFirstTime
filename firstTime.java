4bbd04bb3c8906b4a792e28d305348a28d26d8ba
Chubasyasya <allayarova.anastasiyaa@mail.ru>
2023-09-03 22:17:36 +0300

    creating the basis of the project

---
+++ firstTime.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class firstTime{
+	public static void main(String[] args){
+		String ss = "dkshsa";
+		if (ss.equals("dkshsa")){
+			System.out.print("yes");
+		}else {
+			System.out.print("no");
+		}
+	}
+}
